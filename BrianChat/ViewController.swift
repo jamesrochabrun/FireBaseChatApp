0f29ecbfdc8f45678291f105a0a6c4981de890a8
james <jamesrochabrun@gmail.com>
2017-01-23 20:12:06 -0800

    whatch second video

--- BrianChat/ViewController.swift
+++ BrianChat/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UITableViewController {
+    
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let ref = FIRDatabase.database().reference(fromURL: "https://workchat-fc8ab.firebaseio.com/")
+        ref.updateChildValues(["someValue" : 123])
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
         
